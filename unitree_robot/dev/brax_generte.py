b4b133c11ff38043b3c2fea72ab753512c14154f
freya <freya.aino@pm.me>
2025-09-17 18:42:39 +0200

    added new local dependency lightwheel

--- unitree_robot/dev/brax_generte.py
+++ unitree_robot/dev/brax_generte.py
@@ -1,3 +1,9 @@
-import brax
+from brax.io import mjcf, html
+from pathlib import Path
 
-brax.System
+mjcf_file_path = str(Path("/home/fey/Downloads/test-go2-mjcf-file.xml").resolve())
+
+print(f"loading mjcf file from: {mjcf}")
+scene = mjcf.load(mjcf_file_path)
+
+print(scene)
--- unitree_robot/dev/usd_files.py
+++ unitree_robot/dev/usd_files.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
-usd_file_path = Path("unitree_robot/unitree_model/Go2/usd/go2.usd").resolve()
-urdf_file_path = Path("unitree_robot/unitree_model_extracted/Go2/").resolve()
+usd_file_path = Path("unitree_model/Go2/usd/go2.usd").resolve()
+urdf_file_path = Path("unitree_model_extracted/Go2/").resolve()
 
 print(usd_file_path)
 
-from nvidia.srl.from_usd.to_urdf import UsdToUrdf
+# from nvidia.srl.from_usd.to_urdf import UsdToUrdf
 
-UsdToUrdf.init_from_file(usd_file_path).save_to_file(urdf_file_path)
+# UsdToUrdf.init_from_file(usd_file_path).save_to_file(urdf_file_path)
+
+import lightwheel.srl.from_usd.to_mjcf
