99ed552555e7eccb653b88493b1caead4c0bfc94
freya <freya.aino@pm.me>
2025-09-18 17:02:07 +0200

    included lightwheel usd to mjcf conversion

--- unitree_robot/dev/usd_files.py
+++ unitree_robot/dev/usd_files.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 
-usd_file_path = Path("unitree_model/Go2/usd/go2.usd").resolve()
-urdf_file_path = Path("unitree_model_extracted/Go2/").resolve()
+from lightwheel.srl.from_usd.to_mjcf import UsdToMjcf
+from pxr import Usd
 
-print(usd_file_path)
+usd_source_file_path = Path("external/unitree-robot-models/Go2/usd/go2.usd").resolve()
+target_mjcf_file_path = Path("external/files/Go2/").resolve()
+
+print(f"extracting usd from: {usd_source_file_path}")
+usd_stage = Usd.Stage.Open(str(usd_source_file_path))
+mjcf_file = UsdToMjcf(stage=usd_stage).save_to_file(str(target_mjcf_file_path))
 
 # from nvidia.srl.from_usd.to_urdf import UsdToUrdf
 
