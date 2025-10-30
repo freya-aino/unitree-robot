6779b8167766fa5432c22cc4091749d2bf8c3786
Freya Aino <freya.aino@pm.me>
2025-10-30 13:58:58 +0100

    added experiments and rewards from dennis

--- unitree_robot/train/experiments.py
+++ unitree_robot/train/experiments.py
@@ -1,26 +1,41 @@
 from abc import ABC
 from mujoco import MjData
 
-from unitree_robot.train.rewards import BaseOrientationReward
+from unitree_robot.train.rewards import BaseOrientationReward, EnergyReward, BodyHeightReward, JointLimitReward
 
 class Experiment(ABC):
     def __init__(self):
         pass
-    def calculate_reward(self, **rewards):
+    def __call__(self, **rewards):
         return {k: l for k, l in rewards.items()}
-    def get_reward_shape(self):
+    def __len__(self):
         raise NotImplementedError
 
+
 class StandUpExperiment(Experiment):
-    def __init__(self, body_name: str, body_angle_reward_scale: float):
+    def __init__(
+        self,
+        body_name: str,
+        body_angle_reward_scale: float,
+        body_height_reward_scale: float,
+        energy_reward_scale: float,
+        joint_limit_reward_scale: float
+    ):
+
+        self.base_height_reward = BodyHeightReward(scale=body_height_reward_scale)
         self.base_orientation_reward = BaseOrientationReward(body_name=body_name, scale=body_angle_reward_scale)
+        self.energy_reward = EnergyReward(scale=energy_reward_scale)
+        self.joint_limit_loss = JointLimitReward(scale=joint_limit_reward_scale)
+
         super().__init__()
 
-    def calculate_reward(self, mj_data: MjData):
-        return super().calculate_reward(
-            body_angle = self.base_orientation_reward.calculate_reward(mj_data),
+    def __call__(self, mj_data: MjData):
+        return super()(
+            body_angle = self.base_orientation_reward(mj_data),
+            body_height = self.base_height_reward(mj_data),
+            energy = self.energy_reward(mj_data),
+            joint_limit = self.joint_limit_loss(mj_data),
         )
 
-    def get_reward_shape(self):
-        return [1]
-
+    def __len__(self):
+        return 4
--- unitree_robot/train/rewards.py
+++ unitree_robot/train/rewards.py
@@ -1,5 +1,6 @@
 from abc import ABC
 import numpy as np
+from typing import List
 from mujoco import MjData
 from numpy.typing import NDArray
 from scipy.spatial.transform import Rotation as R
@@ -16,8 +17,10 @@ class Reward(ABC):
     def __init__(self, scale: float = 1.0):
         self.scale = scale
 
-    def calculate_reward(self, data: MjData):
-        pass
+    def __call__(self, reward):
+        return reward * self.scale
+
+
     
 class BaseOrientationReward(Reward):
     def __init__(self, body_name: str, scale: float, angle_vector: NDArray[np.float32] = np.array([1.0,0.0,0.0])):
@@ -25,7 +28,87 @@ class BaseOrientationReward(Reward):
         self.angle_vector = angle_vector
         super().__init__(scale=scale)
 
-    def calculate_reward(self, data: MjData):
+    def __call__(self, data: MjData):
         body_quat = data.body(self.body_name).xquat
         assert body_quat.sum() > 0, "body rotation quaternion is not initialized at this point"
-        return self.scale * calc_angle(body_rotation_quat=body_quat, angle_vector=self.angle_vector)
+        return super()((1 - calc_angle(body_rotation_quat=body_quat, angle_vector=self.angle_vector)))
+
+
+class BodyDistanceReward(Reward):
+
+    def __init__(
+        self,
+        body_names_from: List[str],
+        body_names_to: List[str],
+        scale: float = 1.0
+    ):
+        self.body_names_from = body_names_from
+        self.body_names_to = body_names_to
+        super().__init__(scale=scale)
+
+    def __call__(self, data: MjData):
+        # target_height = 0.3  # TODO
+
+        # global_pos = data.xpos
+        # foot_z_mean_position = np.mean(global_pos[[5, 9, 13, 17], 2])  # global_pos[[5,9,13,17], 2] = z-values of foots
+        # actual_height = global_pos[1, 2] - z_mean  # global_pos[1, 2] = z-value of the basis
+        # error_height = np.abs(actual_height - target_height)
+
+        # weight_height = 1.0 # TODO
+        # loss_height = weight_height * np.abs(error_height)
+
+        # case 1: actual_heigt < target_height -> error_height < 0 -> loss_height > 0
+        # case 2: actual_heigt > target_height -> error_height > 0 -> loss_height > 0
+        # case 3: actual_heigt = target_height -> error_height = 0 -> loss_height = 0
+
+        from_positions = np.stack([data.body(n).xpos for n in self.body_names_from])
+        to_positions = np.stack([data.body(n).xpos for n in self.body_names_from])
+
+        dist = np.mean(np.abs(from_positions.mean(axis=0) - to_positions.mean(axis=0)))
+        return super()(dist)
+
+class BodyHeightReward(BodyDistanceReward):
+    def __init__(
+            self,
+            body_name_from: List[str] = ["base_link"],
+            body_names_to: List[str] = ["FL_foot", "FR_foot", "RL_foot", "RR_foot"],
+            scale: float = 1.0
+    ):
+        super().__init__(
+            body_names_from=body_name_from,
+            body_names_to=body_names_to,
+            scale=scale
+        )
+
+    def __call__(self, data: MjData):
+        return super()(data=data)
+
+
+class EnergyReward(Reward):
+
+    def __init__(self, scale: float = 1.0):
+        super().__init__(scale=scale)
+
+    def __call__(self, data: MjData):
+        actuator_force = np.mean(np.abs(data.actuator_force))
+
+        # weight_energy = 0.1 # TODO
+        # loss_energy = weight_energy * jp.sum(jp.abs(actuator_force))
+
+        return super()(-actuator_force)
+
+
+class JointLimitReward(Reward):
+
+    def __init__(self, scale: float = 1.0):
+        super().__init__(scale=scale)
+
+    def __call__(self, data: MjData):
+        qpos = data.qpos[7:]  # Gibt die Positions-Werte für die 12 Beingelenke aus
+        jnt_range = model.jnt_range[1:]  # Gibt die Gelenk-Limits für die 12 Beingelenke aus
+        jnt_center = (jnt_range[:, 0] + jnt_range[:, 1]) / 2  # jnt_range[:, 0]: Min-Werte aller Gelenke/ jnt_range[:, 1]: Max Werte aller Gelenke
+        jnt_half_range = (jnt_range[:, 1] - jnt_range[:, 0]) / 2  # Berechnet die halbe Reichweite
+        normalized_deviation = (qpos - jnt_center) / jnt_half_range  # Normalisiert die Abweichung zwischen der tatsächlichen Gelenkposition und dessen Center auf -1 bis 1
+        loss = np.mean(np.abs(normalized_deviation))
+
+        return super()(-loss)
